84cdecb4d785ed23eae0bf98118fad51d78ffebe
Duysenaliyev Alibi <40587377+realibi@users.noreply.github.com>
2018-12-29 15:16:54 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Iceberg" rel="stylesheet">
+
+    <link rel="stylesheet" href="index.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="container">
+        <br>
+        <div class="col-xs-12">
+            <div class="logotype"></div>
+        </div>
+        <br>
+        <div class="col-xs-12">
+            <div class="menuItem">ПРАЙС</div>
+        </div>
+        <div class="col-xs-12">
+            <div class="menuItem">ГАЛЕРЕЯ</div>
+        </div>
+        <div class="col-xs-12">
+            <div class="menuItem">ОТЗЫВЫ</div>
+        </div>
+        <div class="col-xs-12">
+            <div class="menuItem">О НАС</div>
+        </div>
+
+        <br>
+        <br>
+
+        <div class="col-xs-12">
+            <div class="line">
+            </div>
+        </div>
+
+        <br>
+        <br>
+
+        <div class="col-xs-12">
+            <div class="circleItem1">
+            </div>
+        </div>
+        <div class="titleItem">ДНЕВНОЙ МАКИЯЖ</div>
+
+        <br>
+        <br>
+
+        <div class="col-xs-12">
+            <div class="line">
+            </div>
+        </div>
+
+        <br>
+        <br>
+
+        <div class="col-xs-12">
+            <div class="circleItem2">
+            </div>
+        </div>
+        <div class="titleItem">КУРСЫ ВИЗАЖА</div>
+
+        <br>
+        <br>
+
+        <div class="col-xs-12">
+            <div class="line">
+            </div>
+        </div>
+
+        <br>
+        <br>
+
+        <div class="col-xs-12">
+            <div class="circleItem3">
+            </div>
+        </div>
+        <div class="titleItem">ВЕЧЕРНИЙ МАКИЯЖ</div>
+
+        <br>
+        <br>
+
+        <div class="col-xs-12">
+            <div class="menuItem">
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+
+</html>
