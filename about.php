19921b865f1fa1c54d277136ba8dbc0ecebbd7c8
Duysenaliyev Alibi <40587377+realibi@users.noreply.github.com>
2018-12-30 00:55:29 +0600

    Add files via upload

--- about.php
+++ about.php
@@ -71,7 +71,7 @@
 
         <div class="col-xs-12">
             <div class="footer">
-                Сарыарка 31/2<br> +77710993580     +77775555787<br> @belleza_makeup_ast
+            Сарыарка 31/2<br> +77710993580 | +77775555787<br> @belleza_makeup_ast
             </div>
         </div>
     </div>
